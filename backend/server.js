43e5e932404e4600548513292c40ecc749117a06
Preksha Maheshwari <prekshamaheshwari6@gmail.com>
2026-05-18 14:32:00 +0530

    Fix production CORS

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: "https://medora-three.vercel.app/",
+    origin: "https://medora-three.vercel.app",
 
     credentials: true
 }));

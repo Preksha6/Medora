a59abef8c72e32a1e760ea3cb87fb61b10f5d4c3
Preksha Maheshwari <prekshamaheshwari6@gmail.com>
2026-05-18 17:25:59 +0530

    Fix JWT middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,19 +1,48 @@
 import jwt from "jsonwebtoken";
 
-const auth = (req, res, next) => {
+const auth = (
+    req,
+    res,
+    next
+) => {
 
-    const token = req.headers.authorization;
+    const authHeader =
+    req.headers.authorization;
 
-    if (!token) {
+    // No header
+
+    if (!authHeader) {
+
         return res.status(401).json({
-            message: "No token"
+
+            message:
+            "No token"
         });
     }
 
     try {
 
-        const decoded = jwt.verify(
+        // Remove "Bearer "
+
+        const token =
+        authHeader.split(" ")[1];
+
+        if (!token) {
+
+            return res.status(401).json({
+
+                message:
+                "Invalid token"
+            });
+        }
+
+        // Verify token
+
+        const decoded =
+        jwt.verify(
+
             token,
+
             process.env.JWT_SECRET
         );
 
@@ -23,8 +52,12 @@ const auth = (req, res, next) => {
 
     } catch (error) {
 
+        console.log(error);
+
         res.status(401).json({
-            message: "Invalid token"
+
+            message:
+            "Invalid token"
         });
     }
 };

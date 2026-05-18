02252767b80ea04b17fd6a4983213d864b71a60a
Preksha Maheshwari <prekshamaheshwari6@gmail.com>
2026-05-18 13:57:34 +0530

    Fixed CORS

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "https://medora-three.vercel.app/",
+
+    credentials: true
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
